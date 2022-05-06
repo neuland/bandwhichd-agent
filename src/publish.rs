d08002bf2c19ca6be843b0bc9dc548c1b26070e7
Michel Zimmer <michel.zimmer@neuland-bfi.de>
2022-05-06 17:33:29 +0200

    fix(message): serialize strings properly

--- src/publish.rs
+++ src/publish.rs
@@ -1,8 +1,10 @@
+use std::net::IpAddr;
+
+use serde::{Serialize, Serializer};
+use time::OffsetDateTime;
+
 use crate::network::Protocol;
 use crate::{OpenSockets, Utilization};
-use serde::{Serialize, Serializer};
-use std::net::IpAddr;
-use std::time::SystemTime;
 
 #[derive(Serialize)]
 #[serde(tag = "version", content = "message")]
@@ -28,69 +30,66 @@ pub struct MessageV1 {
 
 impl MessageV1 {
     pub fn from(agent_name: String, utilization: Utilization, open_sockets: OpenSockets) -> Self {
+        let mut connections: Vec<ConnectionV1> = utilization
+            .connections
+            .into_iter()
+            .map(|(connection, connection_info)| ConnectionV1 {
+                interface_name: connection_info.interface_name,
+                local_ip_address: IpAddressV1(connection.local_socket.ip),
+                local_port: connection.local_socket.port,
+                remote_ip_address: IpAddressV1(connection.remote_socket.ip),
+                remote_port: connection.remote_socket.port,
+                protocol: ProtocolV1(connection.local_socket.protocol),
+                received: BytesCount(connection_info.total_bytes_downloaded),
+                sent: BytesCount(connection_info.total_bytes_uploaded),
+            })
+            .collect();
+        connections.sort();
+        let mut open_sockets: Vec<OpenSocketV1> = open_sockets
+            .sockets_to_procs
+            .into_iter()
+            .map(|(socket, process)| OpenSocketV1 {
+                ip_address: IpAddressV1(socket.ip),
+                port: socket.port,
+                protocol: ProtocolV1(socket.protocol),
+                process,
+            })
+            .collect();
+        open_sockets.sort();
         MessageV1 {
             agent_name,
-            start: TimestampV1(utilization.start),
-            stop: TimestampV1(utilization.stop),
-            connections: utilization
-                .connections
-                .into_iter()
-                .map(|(connection, connection_info)| ConnectionV1 {
-                    interface_name: connection_info.interface_name,
-                    protocol: ProtocolV1(connection.local_socket.protocol),
-                    local_ip_address: IpAddressV1(connection.local_socket.ip),
-                    local_port: connection.local_socket.port,
-                    remote_ip_address: IpAddressV1(connection.remote_socket.ip),
-                    remote_port: connection.remote_socket.port,
-                    received: connection_info.total_bytes_downloaded,
-                    sent: connection_info.total_bytes_uploaded,
-                })
-                .collect(),
-            open_sockets: open_sockets
-                .sockets_to_procs
-                .into_iter()
-                .map(|(socket, process)| OpenSocketV1 {
-                    ip_address: IpAddressV1(socket.ip),
-                    port: socket.port,
-                    protocol: ProtocolV1(socket.protocol),
-                    process,
-                })
-                .collect(),
+            start: TimestampV1(utilization.start.into()),
+            stop: TimestampV1(utilization.stop.into()),
+            connections,
+            open_sockets,
         }
     }
 }
 
-#[derive(Serialize)]
-pub struct ConnectionV1 {
-    pub interface_name: String,
-    pub protocol: ProtocolV1,
-    pub local_ip_address: IpAddressV1,
-    pub local_port: u16,
-    pub remote_ip_address: IpAddressV1,
-    pub remote_port: u16,
-    pub received: u128,
-    pub sent: u128,
-}
-
-#[derive(Serialize)]
-pub struct OpenSocketV1 {
-    pub ip_address: IpAddressV1,
-    pub port: u16,
-    pub protocol: ProtocolV1,
-    pub process: String,
-}
-
-pub struct TimestampV1(SystemTime);
+pub struct TimestampV1(OffsetDateTime);
 
 impl Serialize for TimestampV1 {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        time::serde::rfc3339::serialize(&self.0.into(), serializer)
+        time::serde::rfc3339::serialize(&self.0, serializer)
     }
 }
 
+#[derive(Serialize, Ord, PartialOrd, Eq, PartialEq)]
+pub struct ConnectionV1 {
+    pub interface_name: String,
+    pub local_ip_address: IpAddressV1,
+    pub local_port: u16,
+    pub remote_ip_address: IpAddressV1,
+    pub remote_port: u16,
+    pub protocol: ProtocolV1,
+    pub received: BytesCount,
+    pub sent: BytesCount,
+}
+
+#[derive(Ord, PartialOrd, Eq, PartialEq)]
 pub struct IpAddressV1(IpAddr);
 
 impl Serialize for IpAddressV1 {
@@ -98,10 +97,11 @@ impl Serialize for IpAddressV1 {
     where
         S: Serializer,
     {
-        serializer.serialize_bytes(self.0.to_string().as_bytes())
+        serializer.serialize_str(self.0.to_string().as_str())
     }
 }
 
+#[derive(Ord, PartialOrd, Eq, PartialEq)]
 pub struct ProtocolV1(Protocol);
 
 impl Serialize for ProtocolV1 {
@@ -109,12 +109,195 @@ impl Serialize for ProtocolV1 {
     where
         S: Serializer,
     {
-        serializer.serialize_bytes(
-            match self.0 {
-                Protocol::Tcp => "tcp",
-                Protocol::Udp => "udp",
+        serializer.serialize_str(match self.0 {
+            Protocol::Tcp => "tcp",
+            Protocol::Udp => "udp",
+        })
+    }
+}
+
+#[derive(Ord, PartialOrd, Eq, PartialEq)]
+pub struct BytesCount(u128);
+
+impl Serialize for BytesCount {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(self.0.to_string().as_str())
+    }
+}
+
+#[derive(Serialize, Ord, PartialOrd, Eq, PartialEq)]
+pub struct OpenSocketV1 {
+    pub ip_address: IpAddressV1,
+    pub port: u16,
+    pub protocol: ProtocolV1,
+    pub process: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use std::net::{Ipv4Addr, Ipv6Addr};
+    use std::time::SystemTime;
+
+    use assert_json_diff::assert_json_eq;
+    use serde_json::json;
+    use serde_json::{from_str, Value};
+    use time::macros::datetime;
+
+    use crate::network::{Connection, ConnectionInfo, Socket};
+    use crate::LocalSocket;
+
+    use super::*;
+
+    #[test]
+    fn should_serialize_v1_json() {
+        // given
+        let message = VersionedMessage::from(
+            "some-host.example.com".to_string(),
+            Utilization {
+                connections: HashMap::from([
+                    (
+                        Connection {
+                            remote_socket: Socket {
+                                ip: IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+                                port: 8080,
+                            },
+                            local_socket: LocalSocket {
+                                ip: IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+                                port: 36070,
+                                protocol: Protocol::Tcp,
+                            },
+                        },
+                        ConnectionInfo {
+                            interface_name: "lo".to_string(),
+                            total_bytes_downloaded: 0,
+                            total_bytes_uploaded: 13882,
+                        },
+                    ),
+                    (
+                        Connection {
+                            remote_socket: Socket {
+                                ip: IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+                                port: 36070,
+                            },
+                            local_socket: LocalSocket {
+                                ip: IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+                                port: 8080,
+                                protocol: Protocol::Tcp,
+                            },
+                        },
+                        ConnectionInfo {
+                            interface_name: "lo".to_string(),
+                            total_bytes_downloaded: 608,
+                            total_bytes_uploaded: 0,
+                        },
+                    ),
+                    (
+                        Connection {
+                            remote_socket: Socket {
+                                ip: IpAddr::V4(Ipv4Addr::new(192, 168, 10, 34)),
+                                port: 5353,
+                            },
+                            local_socket: LocalSocket {
+                                ip: IpAddr::V4(Ipv4Addr::new(192, 168, 10, 87)),
+                                port: 43254,
+                                protocol: Protocol::Udp,
+                            },
+                        },
+                        ConnectionInfo {
+                            interface_name: "tun0".to_string(),
+                            total_bytes_downloaded: 120,
+                            total_bytes_uploaded: 64,
+                        },
+                    ),
+                ]),
+                start: SystemTime::from(datetime!(2022-05-06 15:14:51.74223728 utc)),
+                stop: SystemTime::from(datetime!(2022-05-06 15:15:01.74260156 utc)),
+            },
+            OpenSockets {
+                sockets_to_procs: HashMap::from([
+                    (
+                        LocalSocket {
+                            ip: IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0)),
+                            port: 37863,
+                            protocol: Protocol::Tcp,
+                        },
+                        "java".to_string(),
+                    ),
+                    (
+                        LocalSocket {
+                            ip: IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
+                            port: 68,
+                            protocol: Protocol::Udp,
+                        },
+                        "dhclient".to_string(),
+                    ),
+                ]),
+            },
+        );
+
+        // when
+        let result = serde_json::to_string(&message);
+
+        // then
+        let actual: Value = from_str(result.unwrap().as_str()).unwrap();
+        let expected: Value = json!({
+            "version": "1",
+            "message": {
+                "agent_name": "some-host.example.com",
+                "start": "2022-05-06T15:14:51.74223728Z",
+                "stop": "2022-05-06T15:15:01.74260156Z",
+                "connections": [
+                    {
+                        "interface_name": "lo",
+                        "local_ip_address": "127.0.0.1",
+                        "local_port": 8080,
+                        "remote_ip_address": "127.0.0.1",
+                        "remote_port": 36070,
+                        "protocol": "tcp",
+                        "received": "608",
+                        "sent": "0"
+                    },
+                    {
+                        "interface_name": "lo",
+                        "local_ip_address": "127.0.0.1",
+                        "local_port": 36070,
+                        "remote_ip_address": "127.0.0.1",
+                        "remote_port": 8080,
+                        "protocol": "tcp",
+                        "received": "0",
+                        "sent": "13882"
+                    },
+                    {
+                        "interface_name": "tun0",
+                        "local_ip_address": "192.168.10.87",
+                        "local_port": 43254,
+                        "remote_ip_address": "192.168.10.34",
+                        "remote_port": 5353,
+                        "protocol": "udp",
+                        "received": "120",
+                        "sent": "64"
+                    }
+                ],
+                "open_sockets": [
+                    {
+                        "ip_address": "0.0.0.0",
+                        "port": 68,
+                        "protocol": "udp",
+                        "process": "dhclient"
+                    },
+                    {
+                        "ip_address": "::",
+                        "port": 37863,
+                        "protocol": "tcp",
+                        "process": "java"
+                    }
+                ]
             }
-            .as_ref(),
-        )
+        });
+        assert_json_eq!(actual, expected);
     }
 }
